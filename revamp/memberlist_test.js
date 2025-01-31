6420c7346aba967d2d90b7e79349f2faa03a8e0d
unravelgraph <80479658+unravelgraph@users.noreply.github.com>
2025-01-31 11:07:51 -0500

    Create memberlist_test.js

---
+++ revamp/memberlist_test.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+    if (window.location.href.includes("/memberlist")) {
+        var membersPerPage = 15,
+            allMembers = document.createDocumentFragment(),
+            debug = true; // Set to 'true' to enable debug messages, 'false' to disable
+
+        $('.userlist').empty();
+
+        function debugLog(message) {
+            if (debug) {
+                console.log(message);
+            }
+        }
+
+        function loadAllMembers(page) {
+            var pageUrl = '/memberlist?start=' + ((page - 1) * membersPerPage);
+            debugLog("Loading page: " + pageUrl);
+
+            $.ajax({
+                url: pageUrl,
+                success: function(data) {
+                    debugLog("AJAX request succeeded for page: " + page);
+                    var newMembers = $(data).find('.userlist_profil');
+                    debugLog("Found " + newMembers.length + " new members on page " + page);
+
+                    if (newMembers.length === 0) {
+                        displayInitialMembers();
+                        return;
+                    }
+
+                    newMembers.each(function() {
+                        allMembers.append(this);
+                    });
+
+                    loadAllMembers(page + 1);
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    debugLog("Failed to load page " + page + ": " + textStatus + " - " + errorThrown);
+                }
+            });
+        }
+
+        function displayInitialMembers() {
+            debugLog("Displaying all loaded members");
+            $(".userlist").append(allMembers);
+        }
+
+        debugLog("Starting to load all members from page 1");
+        loadAllMembers(1);
+    }
+});
