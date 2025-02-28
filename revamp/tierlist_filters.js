974b87ced74ad18dd184654b452cea8e9dad6b30
unravelgraph <80479658+unravelgraph@users.noreply.github.com>
2025-02-27 21:04:10 -0500

    Update tierlist_filters.js

--- revamp/tierlist_filters.js
+++ revamp/tierlist_filters.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     var $container = $(".allcards");
     var filters = {};
-    var classOrder = ['ten', 's', 'a', 'b', 'c', 't', 'x', 'v'];
+    var classOrder = ['ten', 's', 'a', 'b', 'c', 't', 'x', 'h', 'v'];
 
     var $grid = $container.isotope({
         itemSelector: ".card",
