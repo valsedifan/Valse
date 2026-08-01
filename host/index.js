f3dc5142a87a65d8add9e1252a44e198a7352380
myrtill3 <m.yreil@hotmail.ca>
2026-08-01 13:32:26 -0400

    Update index.js

--- host/index.js
+++ host/index.js
@@ -167,30 +167,3 @@ fs.writeFileSync(
 
 
 console.log("Gallery generated!");
-
-
-document.addEventListener("click", function(e) {
-
-    if (!e.target.classList.contains("copy-btn")) return;
-
-    const url = new URL(
-        e.target.dataset.url,
-        window.location.origin + window.location.pathname
-    ).href;
-
-
-    navigator.clipboard.writeText(url)
-        .then(() => {
-
-            e.target.textContent = "Copied!";
-
-            setTimeout(() => {
-                e.target.textContent = "Copy URL";
-            }, 1500);
-
-        })
-        .catch(err => {
-            console.error("Copy failed:", err);
-        });
-
-});
